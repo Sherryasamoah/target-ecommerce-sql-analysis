286cf5055691b36c6c80ee1ce950b34c2ceb50ad
Sherry Baah Asamoah <baahsasamoah5@gmail.com>
2025-11-09 07:04:15 +0000

    Update Data exploration.sql

--- sql/Data exploration.sql
+++ sql/Data exploration.sql
@@ -10,3 +10,21 @@ LIMIT 10;
 SELECT * 
 FROM `Target_Data.geolocation`
 LIMIT 10;
+
+#2. Get the time range between which all
+  the orders were placed.
+SELECT 
+MIN(order_purchase_timestamp) as start_date,
+MAX (order_purchase_timestamp) as end_date
+FROM `Target_Data.orders`;
+
+#3. Count the Cities & States of customers who ordered during the given period.
+
+SELECT 
+COUNT(c.customer_city),count(c.customer_state)
+
+FROM `Target_Data.orders` as o
+JOIN `Target_Data.customers` as c 
+ON o.customer_id = c.customer_id
+  WHERE EXTRACT (YEAR FROM o.order_purchase_timestamp) = 2017
+  AND EXTRACT (MONTH FROM o.order_purchase_timestamp) between 1 and 12;
