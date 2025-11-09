939b72dd64dfd5ac22fbe57222ff7664dd17a2c9
Sherry Baah Asamoah <baahsasamoah5@gmail.com>
2025-11-09 07:24:54 +0000

    Enhance SQL queries for delivery and logistics analysis
    Added queries to calculate delivery times, average delivery time by state, and average freight values.

--- sql/Delivery & Logistics.sql
+++ sql/Delivery & Logistics.sql
@@ -1,3 +1,20 @@
+# Calculate days/period between purchase time, delivery and estimated delivery
+SELECT 
+order_id,
+TIMESTAMP_DIFF(order_delivered_customer_date, order_purchase_timestamp, DAY) AS days_to_deliver,
+TIMESTAMP_DIFF(order_estimated_delivery_date, order_delivered_customer_date, DAY) AS diff_estimated_delivery,
+ CASE
+    WHEN TIMESTAMP_DIFF(order_estimated_delivery_date, order_delivered_customer_date, DAY) > 0 THEN 'Early'
+    WHEN TIMESTAMP_DIFF(order_estimated_delivery_date, order_delivered_customer_date, DAY) = 0 THEN 'On Time'
+    WHEN TIMESTAMP_DIFF(order_estimated_delivery_date, order_delivered_customer_date, DAY) < 0 THEN 'Late'
+    ELSE 'Unknown'
+  END AS delivery_status
+
+FROM 
+  `Target_Data.orders`;
+
+
+
 #Find out the top 5 states with the highest & lowest average delivery time.
 SELECT 
 c.customer_state,
@@ -24,8 +41,35 @@ GROUP BY customer_state
 ORDER BY avg_delivery_time DESC
 LIMIT 5;
 
+#Find out the top 5 states with the highest & lowest average freight value.  
+SELECT 
+c.customer_state, 
+AVG(oi.freight_value) as avg_freight
+FROM `Target_Data.orders` AS o
+JOIN `Target_Data.order_items` AS oi
+ON o.order_id = oi.order_id
+JOIN `Target_Data.customers` AS c
+ON o.customer_id = c.customer_id
+GROUP BY customer_state
+ORDER BY avg_freight DESC
+LIMIT 5;
 
-#Find out the top 5 states where the order delivery is really fast as compared to the estimated date of delivery.
+#lowest average freight value.  
+
+SELECT 
+c.customer_state, 
+AVG(oi.freight_value) AS avg_freight
+FROM `Target_Data.orders` AS o
+JOIN `Target_Data.order_items` AS oi
+ON o.order_id = oi.order_id
+JOIN `Target_Data.customers` AS c
+ON o.customer_id = c.customer_id
+GROUP BY customer_state
+ORDER BY avg_freight ASC
+LIMIT 5;
+
+
+# Find out the top 5 states where the order delivery is really fast as compared to the estimated date of delivery.
 SELECT 
   c.customer_state,
   ROUND(AVG(TIMESTAMP_DIFF(o.order_estimated_delivery_date, o.order_delivered_customer_date, DAY)), 2) AS avg_days_early
